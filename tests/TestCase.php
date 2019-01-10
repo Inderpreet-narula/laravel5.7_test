05a57a54275f85467bc2c29ce7484d0f4eeaf284
inderpreet singh <inderpreetsingh@ucreate.co.in>
2019-01-10 17:14:03 +0530

    Wrote basic positive test cases

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,11 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+	public function testHelloWorld() {
+		$this->assertTrue(true);
+		$this->assertFalse(false);
+		$this->assertNotEmpty(['hello']);
+		$this->assertEquals(true, true);
+	}
 }
